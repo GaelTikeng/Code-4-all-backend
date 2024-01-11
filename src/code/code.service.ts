48aaaad187cf72f0a59a4d98d302855c11635603
GaelTikeng <Gaelinho@yahoo.fr>
2024-01-11 06:32:23 +0100

    changed allback function for nedpoint code by category

--- src/code/code.service.ts
+++ src/code/code.service.ts
@@ -66,10 +66,10 @@ export class CodeService {
   }
 
   // FIND CODE BY PROGAMMING LANGUAGE
-  async findcodePerCategory(language: string): Promise<Code[]> {
+  async findcodePerCategory(cat: string): Promise<Code[]> {
     const categoryCode = await this.codeModel.findAll({
       where: {
-        programming_language: language
+        category: cat
       },
       include: {
         model: User
