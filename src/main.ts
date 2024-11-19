82f29cdd4476fc21e3a82135279ac3b501da70ec
GaelTikeng <Gaelinho@yahoo.fr>
2024-11-19 21:03:13 +0100

    slight changes

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  const PORT = "3001"
+  const environment = process.env.NODE_ENV || "Development"
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(3001);
+  await app.listen(PORT, async () => {
+    console.log(
+      `Server up and running on ${environment} mode on ${process.env.LOCAL_API_URL}`
+    )
+  });
 }
 bootstrap();
